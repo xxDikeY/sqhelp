5f0d066101596505b4e7e9ffff7517f4d9814b04
xxDikeY <114901775+xxDikeY@users.noreply.github.com>
2024-03-21 06:28:46 +0300

    Update tst.py

--- tst.py
+++ tst.py
@@ -1,13 +1,1 @@
-from main import *
-import sqlite3
 
-database = SqHelp()
-database.path = "test.db"
-
-database.table_add("People")
-database.column_add_int("id")
-database.column_add_str("name")
-
-database.create_table()
-
-print("done")
